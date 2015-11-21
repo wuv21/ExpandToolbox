6578d47c17d3ea276247316e9b3048f53bfde0f9
Vincent Wu <wuv21@uw.edu>
2015-11-21 12:35:07 -0800

    added in weather fonts

--- js/script.js
+++ js/script.js
@@ -14,12 +14,16 @@ angular.module('WeatherApp', ['ui.router', 'chart.js'])
 
         $urlRouterProvider.otherwise('/welcome');
     })
+    .factory('currentWeatherReport', function($http) {
+        return $http.get("http://api.openweathermap.org/data/2.5/weather?id=2172797&appid=2de143494c0b295cca9337e1e96b00e0");
+    })
     .controller('WelcomeController', function($scope) {
         $scope.user = {};
     })
     .controller('WeatherController', function($scope, $http) {
         $scope.user = {name: '', location: ''};
 
+        // Personalized date and greeting
         var date = new Date();
         var daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
@@ -35,15 +39,23 @@ angular.module('WeatherApp', ['ui.router', 'chart.js'])
 
         $scope.dayOfWeek = daysOfWeek[date.getDay()];
 
-        $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
-        $scope.series = ['Series A', 'Series B'];
-        $scope.data = [
-            [65, 59, 80, 81, 56, 55, 40],
-            [28, 48, 40, 19, 86, 27, 90]
-        ];
+        // Wind radar variables
+        $scope.labels = _.fill(Array(36), '');
+        $scope.wind = _.fill(Array(36), 0);
 
+        //Testing factory
+        //currentWeatherReport.success(function(response) {
+        //    console.log(response);
+        //});
+
+        // Get weather data based on city
         $http.get("http://api.openweathermap.org/data/2.5/weather?id=2172797&appid=2de143494c0b295cca9337e1e96b00e0")
             .success(function(response) {
-                console.log(response);
-            })
+                $scope.currentWeather = {description: response.weather[0].description.toLowerCase() || "___",
+                                        currentTemp: (response.main.temp * (9/5) - 459.67).toFixed(1) || "___",
+                                        city: response.name || "___",
+                                        country: response.sys.country || "___",
+                                        wind: $scope.wind[Math.round(response.wind.deg / 10)] = response.wind.speed};
+
+        });
     });
