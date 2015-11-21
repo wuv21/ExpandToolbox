3a97d12aa4a6711019376f0925024c444d20be1f
Vincent Wu <wuv21@uw.edu>
2015-11-20 23:00:43 -0800

    added chart and angular-chart and ran sample code

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
-angular.module('WeatherApp', ['ui.router'])
+angular.module('WeatherApp', ['ui.router', 'chart.js'])
     .config(function($stateProvider, $urlRouterProvider) {
         $stateProvider
             .state('welcome', {
                 url: '/welcome',
                 templateUrl: 'views/welcome.html',
-                controller: 'WeatherController'
+                controller: 'WelcomeController'
             })
             .state('weather', {
                 url: '/weather',
@@ -14,8 +14,19 @@ angular.module('WeatherApp', ['ui.router'])
 
         $urlRouterProvider.otherwise('/welcome');
     })
+    .controller('WelcomeController', function($scope) {
+        $scope.user = {};
+    })
     .controller('WeatherController', function($scope) {
         $scope.user = {name: '', location: ''};
 
-        $('h1').fadeIn(5000);
+
+        $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
+        $scope.series = ['Series A', 'Series B'];
+        $scope.data = [
+            [65, 59, 80, 81, 56, 55, 40],
+            [28, 48, 40, 19, 86, 27, 90]
+        ];
+
+
     });
