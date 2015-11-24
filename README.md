838744a421f948d6f542ef74af29023f665b7d51
Vincent Wu <wuv21@uw.edu>
2015-11-23 22:25:48 -0800

    fixed various bugs, added refresh button, wrote README.md

--- README.md
+++ README.md
@@ -1,8 +1,43 @@
 # ExpandToolbox
-Expanding my javascript toolbox with Chart.js
+Expanding my javascript toolbox with Chart.js, lodash, and angular-chart.js. Welcome to Raindrop!
 
-## Project overview
-This challenge is to utilize Chart.js to create a responsive and modern weather information site based on the user's current location or an user-inputted location. It is the goal for this website to serve as a welcome screen when opening the browser.
+## What is my challenge (Raindrop)?
+This challenge is to utilize Chart.js, Angular-chart.js, and Angular to create a responsive and modern weather information site based on the user's current location or an user-inputted location via the OpenWeatherMap API. This project will also utilize lodash.js and ui-routing for quicker data crunching and seamless interactions. For styling purposes, Bootstrap is used.
 
-## Details
+## Why these technologies?
+Data visualization is a hot topic nowadays and it serves the saying that a picture is worth a thousand words. Data, when organized and presented in a certain, provides information to the user. By combining Chart.js (which is a library on top of D3.js) with Angular, and lodash, a web developer can quickly harness data and present that data in a powerful web app. The latter library provides functions to quickly deal with working with collections and pulling out specific information from objects.
 
+## Are there any potential cautions?
+Chart.js is a popular library with over 17 thousand stars and 57 contributors. It currently has 393 issues open with the latest commit being a week ago. The wrapper for Chart.js for Angular has about 1.2 thousand stars with 22 issues open. Their latest commit was nine hours ago. By using Angular, it does create limitations on what graphs can be created. Using just Chart.js (and by extension, using just D3.js) would offer a greater breadth of options and possibilities in representing data. Lodash, on the other hand, will run well with or without Angular. This library currently has over 12 thousand stars with 0 issues open - with a commit occurring today.
+
+## What is this app?
+This app (called Raindrop) is a weather app that will:
+    1. Ask user for location
+    2. Get weather information based on that location from OpenWeatherMap API
+    3. Load the data into a user-friendly interface.
+
+Since weather data is quite functional in graphs, I used the data visualization libraries to simplify the data and allow the user to quickly get what he or she needs without having to pore through textual or numerical data. This ranges from using a radar graph to see where and how strong the wind is blowing - which is more intuitive way than saying "`NNE at 5mph`"). Chart.js allows interaction with the graphs such as hovering over a point/area to see what that point/area means.
+
+## How to run Raindrop?
+All that is needed to run Raindrop is to visit the [website](http://students.washington.edu/wuv21/ExpandToolbox/#/welcome).
+
+However, more steps are required if re-creating Raindrop. Libraries (in this order) that must be downloaded or linked via a CDN are stated below.
+* Chart.js
+* lodash.js
+* angular.js
+* angular-ui-router.js
+* angular-chart.js
+
+In addition, one must sign up for an OpenWeatherMap API key located [here](http://openweathermap.org/api). To allow for user to input a location based on a city rather than a long numerical id, I used their city names/city id json file located [here](http://bulk.openweathermap.org/sample/). Since the file size is relatively big for the world cities, I decided to use only the US cities one. In using the `city.list.us.json.gz` file, this file must be uncompressed. I additionally shrunk and corrected the format of the json file with my python program that I included in the json folder of this repository. It is advised to run this python program once to generate a compressed json prior to deploying the final product.
+
+## What to do if I want to learn more?
+Definitely check out the documentation! They often have examples and the links to their repositories on GitHub. I have included links below and also a link to the Weather Icons that I used for this app.
+* [Chart.js](http://www.chartjs.org/)
+* [lodash.js](https://lodash.com/)
+* [angular.js](https://angularjs.org/)
+* [angular-ui-router.js](http://angular-ui.github.io/ui-router/site/#/api/ui.router)
+* [angular-chart.js](http://jtblin.github.io/angular-chart.js/)
+* [Bootstrap](http://getbootstrap.com/getting-started/)
+* [Weather Icons](https://erikflowers.github.io/weather-icons/)
+
+Hope you enjoy Raindrop!
--- js/script.js
+++ js/script.js
@@ -3,7 +3,6 @@ angular.module('WeatherApp', ['ui.router', 'chart.js'])
     .constant('userKey', 'user')
     .service('userInfo', function() {
         var user = {};
-
         return user;
     })
     .config(function($stateProvider, $urlRouterProvider) {
@@ -27,6 +26,11 @@ angular.module('WeatherApp', ['ui.router', 'chart.js'])
                 url: '/forecast',
                 templateUrl: 'views/forecast.html',
                 controller: 'WeatherController'
+            })
+            .state('errormessage', {
+                url: '/error',
+                templateUrl: '/error.html',
+                controller: 'WeatherController'
             });
 
         $urlRouterProvider.otherwise('/welcome');
@@ -43,123 +47,151 @@ angular.module('WeatherApp', ['ui.router', 'chart.js'])
         // Submit button f(x) to see weather. Also saves to localstorage
         $scope.view = function() {
             localStorage.setItem(userKey, angular.toJson($scope.user));
-            console.log('click');
             $state.go('weather.current');
         };
     })
     .controller('WeatherController', function($scope, $http, openWeatherAPIkey, userInfo, userKey, $state) {
-        // Get user inputted info
-        $scope.user = angular.fromJson(localStorage.getItem(userKey)) || userInfo;
-        if (!$scope.user.name) {
-            $state.go('welcome');
-        }
-
         // Personalized date and greeting
-        var date = new Date();
-        $scope.daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+        $scope.greeting = function() {
+            var date = new Date();
+            $scope.daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
-        $scope.timeOfDay = 'day';
-        $scope.hour = date.getHours();
-        $scope.isEvening = false;
-        if ($scope.hour < 12) {
-            $scope.timeOfDay = 'morning';
-        } else if ($scope.hour < 18) {
-            $scope.timeOfDay = 'afternoon';
-            $scope.isEvening = true;
-        } else if ($scope.hour < 24) {
-            $scope.timeOfDay = 'evening';
-        }
+            $scope.timeOfDay = 'day';
+            $scope.hour = date.getHours();
+            $scope.isEvening = false;
+            if ($scope.hour < 12) {
+                $scope.timeOfDay = 'morning';
+            } else if ($scope.hour < 18) {
+                $scope.timeOfDay = 'afternoon';
+                $scope.isEvening = true;
+            } else if ($scope.hour < 24) {
+                $scope.timeOfDay = 'evening';
+            }
 
-        $scope.dayOfWeek = $scope.daysOfWeek[date.getDay()];
+            // Days of week
+            $scope.dayOfWeek = $scope.daysOfWeek[date.getDay()];
+        };
 
         // Wind radar variables
         $scope.labels = _.fill(Array(36), '');
         $scope.wind = _.fill(Array(36), 0);
 
+        // Converts Kelvin to Farenheit
         function convertKtoF(n) {
             return (n * (9/5) - 459.67).toFixed(1)
         }
 
         // Get weather data based on city
-        if ($scope.user.loc.id) {
-            $http.get("http://api.openweathermap.org/data/2.5/weather?id=" + $scope.user.loc.id + "&appid=" + openWeatherAPIkey)
+        $scope.getCurrent = function() {
+            if ($scope.user.loc.id) {
+                $http.get("http://api.openweathermap.org/data/2.5/weather?id=" + $scope.user.loc.id + "&appid=" + openWeatherAPIkey)
+                    .then(function(response) {
+                        response = response.data;
+
+                        if (response.cod === '404') {
+                            console.log(response.message);
+                            $state.go('errormessage');
+                        } else {
+                            // Current weather data
+                            $scope.currentWeather = {description: response.weather[0].description.toLowerCase() || "___",
+                                id: response.weather[0].id,
+                                currentTemp: convertKtoF(response.main.temp) || "___",
+                                city: response.name || "___",
+                                country: response.sys.country || "___",
+                                wind: $scope.wind[Math.round(response.wind.deg / 10)] = (response.wind.speed / 1609.34 * 3600).toFixed(2),
+                                sunrise: new Date(response.sys.sunrise * 1000),
+                                sunset: new Date(response.sys.sunset * 1000),
+                                humidity: response.main.humidity,
+                                pressure: response.main.pressure,
+                                cloud: [response.clouds['all'], 100-response.clouds['all']],
+                                rain: response.hasOwnProperty('rain') ? response.rain['3h'] : 0,
+                                snow: response.hasOwnProperty('snow') ? response.snow['3h'] : 0};
+
+                            // Rain/snow data
+                            $scope.rainSnowData = [[$scope.currentWeather.rain, $scope.currentWeather.snow]];
+                            $scope.rainSnowLabels = ['Rain', 'Snow'];
+                        }
+                    });
+            }
+        };
+
+        // Cloud chart data
+        $scope.cloudLabels = ['cloudy', 'clear'];
+        $scope.cloudColors = ['#C5C5C5', '#CBDDE6'];
+
+        // Get weather forecast
+        $scope.getForecast = function() {
+            $http.get("http://api.openweathermap.org/data/2.5/forecast/daily?id=5809844&appid=" + openWeatherAPIkey)
                 .then(function(response) {
                     response = response.data;
 
                     if (response.cod === '404') {
                         console.log(response.message);
+                        $state.go('errormessage');
                     } else {
-                        $scope.currentWeather = {description: response.weather[0].description.toLowerCase() || "___",
-                            id: response.weather[0].id,
-                            currentTemp: convertKtoF(response.main.temp) || "___",
-                            city: response.name || "___",
-                            country: response.sys.country || "___",
-                            wind: $scope.wind[Math.round(response.wind.deg / 10)] = (response.wind.speed / 1609.34 * 3600).toFixed(2),
-                            sunrise: new Date(response.sys.sunrise * 1000),
-                            sunset: new Date(response.sys.sunset * 1000),
-                            humidity: response.main.humidity,
-                            pressure: response.main.pressure,
-                            cloud: [response.clouds['all'], 100-response.clouds['all']],
-                            rain: response.hasOwnProperty('rain') ? response.rain['3h'] : 0,
-                            snow: response.hasOwnProperty('snow') ? response.snow['3h'] : 0};
+                        var forecast = response.list;
 
-                            // Rain/snow data
-                            $scope.rainSnowData = [[$scope.currentWeather.rain, $scope.currentWeather.snow]];
-                            $scope.rainSnowLabels = ['Rain', 'Snow'];
+                        // Get dates of days that are in forecast
+                        var dates = _.pluck(forecast, 'dt');
+
+                        // Format dates for the graph
+                        $scope.forecastLabels = _.map(dates, function(x) {
+                            var date = new Date(x * 1000);
+
+                            return date.toDateString();
+                        });
+
+                        // Series for high and low temperatures for each day
+                        $scope.forecastSeries = ['high', 'low'];
+
+                        // Mine temperature data from API response. Collects high and low temperatures for each day.
+                        $scope.forecastData = [_.pluck(forecast, 'temp.max').map(convertKtoF),
+                            _.pluck(forecast, 'temp.min').map(convertKtoF)];
+
+                        // Graph colors
+                        $scope.forecastColors = ['#E74C3C', '#3498DB'];
+
+                        // Mine description data from API response.
+                        var forecastDesc = _.pluck(forecast, 'weather[0].id');
+
+                        // Format dates for the weather panel
+                        $scope.forecastWeatherDates = _.map(dates, function(x) {return new Date(x * 1000)});
+                        $scope.forecastWeather = [];
+
+                        var idx;
+                        for(idx = 0; idx < $scope.forecastLabels.length; idx++) {
+                            $scope.forecastWeather.push({date: $scope.forecastWeatherDates[idx],
+                                id: forecastDesc[idx]});
+                        }
+
+                        // Mine humidity data
+                        $scope.forecastHumidity = [_.pluck(forecast, 'humidity').slice(0,4)];
+
+                        // Mine pressure data
+                        $scope.forecastPressure = [_.pluck(forecast, 'pressure').slice(0,4)];
+
+                        // Modified labels since humidity & pressure have fewer predicted dates
+                        $scope.forecastLabelsSlice = _.slice($scope.forecastLabels, 0, 4);
                     }
-                });
+            });
+        };
+
+        // Get user inputted info and run page
+        $scope.user = userInfo || angular.fromJson(localStorage.getItem(userKey));
+
+        // Loads weather page only if user exists
+        if (!$scope.user.name) {
+            $state.go('welcome');
+        } else {
+            $scope.greeting();
+            $scope.getCurrent();
+            $scope.getForecast();
         }
 
-        // Cloud chart data
-        $scope.cloudLabels = ['cloudy', 'clear'];
-        $scope.cloudColors = ['#F7F7F7', '#CBDDE6'];
-
-        // Get weather forecast
-        $http.get("http://api.openweathermap.org/data/2.5/forecast/daily?id=5809844&appid=" + openWeatherAPIkey)
-            .success(function(response) {
-                // TODO error catching for http get
-                var forecast = response.list;
-
-                // Get dates of days that are in forecast
-                var dates = _.pluck(forecast, 'dt');
-
-                // Format dates for the graph
-                $scope.forecastLabels = _.map(dates, function(x) {
-                    var date = new Date(x * 1000);
-
-                    return date.toDateString();
-                });
-
-                // Series for high and low temperatures for each day
-                $scope.forecastSeries = ['high', 'low'];
-
-                // Mine temperature data from API response. Collects high and low temperatures for each day.
-                $scope.forecastData = [_.pluck(forecast, 'temp.max').map(convertKtoF),
-                                        _.pluck(forecast, 'temp.min').map(convertKtoF)];
-
-                // Graph colors
-                $scope.forecastColors = ['#E74C3C', '#3498DB'];
-
-                // Mine description data from API response.
-                var forecastDesc = _.pluck(forecast, 'weather[0].id');
-
-                // Format dates for the weather panel
-                $scope.forecastWeatherDates = _.map(dates, function(x) {return new Date(x * 1000)});
-                $scope.forecastWeather = [];
-
-                var idx;
-                for(idx = 0; idx < $scope.forecastLabels.length; idx++) {
-                    $scope.forecastWeather.push({date: $scope.forecastWeatherDates[idx],
-                                                id: forecastDesc[idx]});
-                }
-
-                // Mine humidity data
-                $scope.forecastHumidity = [_.pluck(forecast, 'humidity').slice(0,4)];
-
-                // Mine pressure data
-                $scope.forecastPressure = [_.pluck(forecast, 'pressure').slice(0,4)];
-
-                // Modified labels since humidity & pressure have fewer predicted dates
-                $scope.forecastLabelsSlice = _.slice($scope.forecastLabels, 0, 4);
-            });
+        // Refreshes data upon user click
+        $scope.refresh = function() {
+            $scope.greeting();
+            $scope.getCurrent();
+            $scope.getForecast();
+        }
     });
